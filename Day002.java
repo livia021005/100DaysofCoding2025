0dda91ada81e4c562e048602047f09f605bb23e1
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-09-09 21:17:01 +0800

    Create Day002.java

---
+++ Day002.java
@@ -0,0 +1,14 @@
+package day002;
+
+public class day002 {
+    public static void main(String[] args) {
+        
+        System.out.print("nama saya ");
+        System.out.print("Oliv\n");
+
+        System.out.println("Nim : D0223018");
+        
+        int tb = 162;
+        System.out.printf("Tinggi Badan : %d", tb);
+    }
+}
