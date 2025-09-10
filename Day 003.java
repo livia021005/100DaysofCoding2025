2a032af20931f9ff128484c288896cb18b0be06c
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-09-10 22:14:55 +0800

    Create Day 003.java

---
+++ Day 003.java
@@ -0,0 +1,25 @@
+/**
+ * Contoh Program Java dengan 3 jenis komentar
+ * Program ini dibuat oleh Oliv
+ */
+
+public class day003 {
+    public static void main(String[] args) {
+        
+        // Komentar satu baris
+        System.out.println("Halo, ini contoh komentar satu baris");
+
+        /* 
+           Komentar multi baris
+           Bagian ini menjelaskan bahwa kode berikut 
+           akan menampilkan biodata sederhana.
+        */
+        String nama = "Oliv";
+        String nim = "D0223018";
+        int tinggi = 162;
+
+        System.out.println("Nama   : " + nama);
+        System.out.println("NIM    : " + nim);
+        System.out.println("Tinggi : " + tinggi + " cm");
+    }
+}
