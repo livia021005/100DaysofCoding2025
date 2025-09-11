9356f58820328a4d8cea30fb0ebc1d614842bde3
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-09-11 18:58:13 +0800

    Create Day 004.java

---
+++ Day 004.java
@@ -0,0 +1,17 @@
+public class day004 {
+    public static void main(String[] args) {
+        
+        // \n = baris baru
+        System.out.println("Halo\nIni biodata saya");
+
+        // \t = tab (spasi lebar)
+        System.out.println("Nama\t: Oliv");
+        System.out.println("NIM\t: D0223018");
+
+        // \" = menampilkan tanda kutip
+        System.out.println("Oliv berkata: \"saya dari Universitas Sulawesi Barat\"");
+
+        // \\ = menampilkan backslash
+        System.out.println("Contoh path: C:\\Users\\Oliv\\Documents");
+    }
+}
