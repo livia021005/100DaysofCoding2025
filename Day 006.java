4bd591252a1f0182422848f3d118b7bced8cf93d
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-09-13 02:34:47 +0800

    Update Day 006.java

--- Day 006.java
+++ Day 006.java
@@ -1,14 +1,10 @@
 public class day006 {
 
     public static void main(String[] args) {
-        // float: angka pecahan, butuh huruf f/F di akhir
-        float berat = 45.5f;
+        float berat = 49f;
+        double tinggi = 162.75;
 
-        // double: angka pecahan lebih presisi (default untuk pecahan)
-        double panjang = 162.75;
-
-        // menampilkan hasil
-        System.out.println("Berat   : " + berat + " kg");
-        System.out.println("Panjang : " + panjang + " cm");
+        System.out.println("Berat Badan  : " + berat + " kg");
+        System.out.println("Tinggi Badan : " + tinggi + " cm");
     }
 }
