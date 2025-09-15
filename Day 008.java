dba04b2dd0a20625e0ad423855d2580c3e39f630
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-09-15 20:45:23 +0800

    Create Day 008.java

---
+++ Day 008.java
@@ -0,0 +1,14 @@
+public class day008 {
+    public static void main(String[] args) {
+        String nama = "Oliv";
+        String nim = "D0223018";
+        String jurusan = "Teknik Informatika";
+        String kampus = "Universitas Sulawesi Barat";
+
+        System.out.println("=== Biodata Mahasiswa ===");
+        System.out.println("Nama    : " + nama);
+        System.out.println("NIM     : " + nim);
+        System.out.println("Jurusan : " + jurusan);
+        System.out.println("Kampus  : " + kampus);
+    }
+}
