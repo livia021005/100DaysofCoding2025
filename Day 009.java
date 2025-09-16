291a5b2f7dcef5fef08819c4824215b8dc690da1
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-09-16 20:56:55 +0800

    Create Day 009.java

---
+++ Day 009.java
@@ -0,0 +1,12 @@
+public class day009 {
+    public static void main(String[] args) {
+        int umur;
+        umur = 19;
+
+        System.out.println("Umur awal : " + umur);
+
+        umur = 20;
+
+        System.out.println("Umur setelah update : " + umur);
+    }
+}
