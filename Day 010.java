ee12f7361ba6c91472293aaac0e27b36968ef780
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-09-17 21:17:34 +0800

    Create Day 010.java

---
+++ Day 010.java
@@ -0,0 +1,13 @@
+public class day010 {
+    public static void main(String[] args) {
+        final int TAHUN_LAHIR = 2005; 
+        int umur = 18;                
+
+        System.out.println("Tahun Lahir : " + TAHUN_LAHIR);
+        System.out.println("Umur Awal   : " + umur);
+
+        umur = 19;
+
+        System.out.println("Umur Update : " + umur);
+    }
+}
