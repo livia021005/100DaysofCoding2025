47e3c800f457d7ab234dafc9978c0e9174e6a0f9
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-09-21 18:57:05 +0800

    Create Day 014.java

---
+++ Day 014.java
@@ -0,0 +1,12 @@
+public class day014 {
+    public static void main(String[] args) {
+        String angka1 = "25";
+        String angka2 = "3.14";
+
+        int a = Integer.parseInt(angka1);
+        double b = Double.parseDouble(angka2);
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
