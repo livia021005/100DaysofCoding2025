24d2ca7fee907e638d382f0e5d7409c5ea9894b5
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-09-22 22:38:16 +0800

    Create Day 015.java

---
+++ Day 015.java
@@ -0,0 +1,12 @@
+public class Day015 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 7;
+
+        int hasilTambah = a + b;
+        int hasilKurang = a - b;
+
+        System.out.println("Hasil Penjumlahan: " + hasilTambah);
+        System.out.println("Hasil Pengurangan: " + hasilKurang);
+    }
+}
