b4865dd91710314578c5267946718453b8f7fc5d
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-09-23 19:44:58 +0800

    Create Day 016.java

---
+++ Day 016.java
@@ -0,0 +1,12 @@
+public class day016 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 4;
+
+        int perkalian = a * b;
+        int pembagian = a / b;
+
+        System.out.println("Hasil perkalian: " + perkalian);
+        System.out.println("Hasil pembagian: " + pembagian);
+    }
+}
