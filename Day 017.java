0adc97763f3501f907ce4fc3079e4c514371927b
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-09-24 20:34:48 +0800

    Create Day 017.java

---
+++ Day 017.java
@@ -0,0 +1,10 @@
+public class day017 {
+    public static void main(String[] args) {
+        int a = 22;
+        int b = 5;
+
+        int sisaBagi = a % b;
+
+        System.out.println("Sisa bagi : " + sisaBagi);
+    }
+}
