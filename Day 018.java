93a21e71584a0efca6db16411b8995104a46d911
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-09-25 20:36:11 +0800

    Create Day 018.java

---
+++ Day 018.java
@@ -0,0 +1,15 @@
+public class day018 {
+    public static void main(String[] args) {
+        byte a = 25;
+        int b = a;
+        long c = b;
+        float d = c;
+        double e = d;
+
+        System.out.println("byte   : " + a);
+        System.out.println("int    : " + b);
+        System.out.println("long   : " + c);
+        System.out.println("float  : " + d);
+        System.out.println("double : " + e);
+    }
+}
