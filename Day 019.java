56b3805c4c3903dd12faae2ed36c30453c766c49
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-09-26 20:21:53 +0800

    Create Day 019.java

---
+++ Day 019.java
@@ -0,0 +1,11 @@
+public class day019 {
+    public static void main(String[] args) {
+        double a = 15.75;         // tipe data besar
+        int b = (int) a;          // dikonversi manual ke int
+        byte c = (byte) b;        // dikonversi manual ke byte
+
+        System.out.println("double : " + a);
+        System.out.println("int    : " + b);
+        System.out.println("byte   : " + c);
+    }
+}
