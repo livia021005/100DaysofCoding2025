18e3efc9a777ddb6d3af2f31b7cf967580ebeccf
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-09-26 21:51:12 +0800

    Create Dayy 019.java

---
+++ Dayy 019.java
@@ -0,0 +1,9 @@
+public class day019 {
+    public static void main(String[] args) {
+        double a = 1500.99;      // tipe data besar
+        int b = (int) a;         // dikonversi manual ke int
+
+        System.out.println("double : " + a);
+        System.out.println("int    : " + b);
+    }
+}
