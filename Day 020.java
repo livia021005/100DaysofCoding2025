f9b7efb64fa2aa9d81f2623a81d90f6d362a6bca
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-09-27 16:39:33 +0800

    Create Day 020.java

---
+++ Day 020.java
@@ -0,0 +1,12 @@
+public class day020 {
+    public static void main(String[] args) {
+        int angka = 25;
+        double pecahan = 7.5;
+
+        String a = String.valueOf(angka);
+        String b = String.valueOf(pecahan);
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
