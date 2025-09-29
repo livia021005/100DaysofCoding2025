e98dc1fcfa68b382157cce9159e59912bc63f4c7
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-09-29 20:57:32 +0800

    Create Day 022 Luas persegi.java

---
+++ Day 022 Luas persegi.java
@@ -0,0 +1,9 @@
+public class Day022 {
+    public static void main(String[] args) {
+        int sisi = 9;
+        int luas = sisi * sisi;
+
+        System.out.println("Sisi persegi = " + sisi);
+        System.out.println("Luas persegi = " + luas);
+    }
+}
