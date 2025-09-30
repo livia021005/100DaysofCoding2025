f77578c6fa3302ee2c31e58e4f4ee98ab0d21be8
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-09-30 22:26:21 +0800

    Create Day 023 luas persegi panjang.java

---
+++ Day 023 luas persegi panjang.java
@@ -0,0 +1,11 @@
+public class Day023 {
+    public static void main(String[] args) {
+        int panjang = 8;
+        int lebar = 5;
+
+        int luas = panjang * lebar;
+
+        System.out.println("Panjang: " + panjang);
+        System.out.println("Luas persegi panjang: " + luas);
+    }
+}
