0d543ff654f5d91d2453c9717a41c19d9859d270
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-10-01 23:23:42 +0800

    Create Day 024 Luas lingkaran.java

---
+++ Day 024 Luas lingkaran.java
@@ -0,0 +1,11 @@
+public class Day024 {
+    public static void main(String[] args) {
+        double phi = 3.14;          
+        double r = 8;               
+        double hasil = phi * r * r; 
+
+        System.out.println("Phi = " + phi);
+        System.out.println("Jari-jari lingkaran = " + r + " cm");
+        System.out.println("Luas lingkaran = " + hasil + " cm^2");
+    }
+}
