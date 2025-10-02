9fc830a2336cfd493b70ca1d673fe7820a1f6ec5
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-10-02 20:09:19 +0800

    Create Day 025.java

---
+++ Day 025.java
@@ -0,0 +1,12 @@
+public class Day025 {
+    public static void main(String[] args) {
+        int angka = 20;
+
+        System.out.println("Nilai awal: " + angka);
+        angka++;
+        System.out.println("Setelah increment (angka++): " + angka);
+        angka--;
+        System.out.println("Setelah decrement (angka--): " + angka);
+    }
+}
+
