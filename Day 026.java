5f8e9feb48213740603cb817397924e8a0c69351
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-10-03 17:30:34 +0800

    Create Day 026.java

---
+++ Day 026.java
@@ -0,0 +1,22 @@
+public class Day026 {
+    public static void main(String[] args) {
+        int angka = 20;
+
+        System.out.println("Nilai awal: " + angka);
+
+        angka += 5;
+        System.out.println("Setelah += 5 : " + angka);
+
+        angka -= 3;
+        System.out.println("Setelah -= 3 : " + angka);
+
+        angka *= 2;
+        System.out.println("Setelah *= 2 : " + angka);
+
+        angka /= 4;
+        System.out.println("Setelah /= 4 : " + angka);
+
+        angka %= 3;
+        System.out.println("Setelah %= 3 : " + angka);
+    }
+}
