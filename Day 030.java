3c66cbc6c4bb4b9c82f17199548ed58e860ec242
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-10-07 23:50:01 +0800

    Create Day 030.java

---
+++ Day 030.java
@@ -0,0 +1,12 @@
+public class Day030 {
+    public static void main(String[] args) {
+        int angka1 = 8;
+        int angka2 = 10;
+
+        boolean hasil1 = (angka1 <= angka2);
+        boolean hasil2 = (angka1 >= angka2);
+
+        System.out.println("Apakah angka1 <= angka2? " + hasil1);
+        System.out.println("Apakah angka1 >= angka2? " + hasil2);
+    }
+}
