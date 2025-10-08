4397307535884103038c91eea7c48880b87c6efa
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-10-08 21:46:47 +0800

    Create Day 031.java

---
+++ Day 031.java
@@ -0,0 +1,10 @@
+public class Day031 {
+    public static void main(String[] args) {
+        int angka1 = 4;
+        int angka2 = 20;
+
+        boolean hasil = (angka1 > 5) && (angka2 < 15);
+
+        System.out.println(hasil);
+    }
+}
