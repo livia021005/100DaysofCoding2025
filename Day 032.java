abc4563d5b325c8b67cd2d204d7790075795500e
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-10-09 22:12:57 +0800

    Create Day 032.java

---
+++ Day 032.java
@@ -0,0 +1,9 @@
+public class Day032 {
+    public static void main(String[] args) {
+        boolean a = false;
+        boolean b = true;
+
+        System.out.println(a || b);
+        System.out.println(a || a);
+    }
+}
