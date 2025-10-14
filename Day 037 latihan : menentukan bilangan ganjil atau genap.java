eb4ad795003671424e71081efe62004923110015
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-10-14 20:38:19 +0800

    Create Day 037 latihan : menentukan bilangan ganjil atau genap.java

---
+++ Day 037 latihan : menentukan bilangan ganjil atau genap.java
@@ -0,0 +1,11 @@
+public class Day037 {
+    public static void main(String[] args) {
+        int angka = 8;
+
+        if (angka % 2 == 0) {
+            System.out.println("Bilangan genap");
+        } else {
+            System.out.println("Bilangan ganjil");
+        }
+    }
+}
