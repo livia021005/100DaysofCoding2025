5ac3fbe412e5bdf446b5812a81bf3f8213442094
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-10-26 20:49:59 +0800

    Create Day 049.java

---
+++ Day 049.java
@@ -0,0 +1,9 @@
+public class Day049 {
+    public static void main(String[] args) {
+        int angka = 7;
+
+        String hasil = (angka % 2 == 0) ? "Bilangan genap" : "Bilangan ganjil";
+
+        System.out.println(hasil);
+    }
+}
