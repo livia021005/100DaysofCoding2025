3969c5ab945c8c6374987ee90113d6043c3151b4
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-10-28 22:05:48 +0800

    Create Day 051.java

---
+++ Day 051.java
@@ -0,0 +1,7 @@
+public class Day050 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Perulangan ke-" + i);
+        }
+    }
+}
