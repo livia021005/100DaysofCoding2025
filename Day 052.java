a908118755fb8992814958fbbdc08c44a02c1d1b
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-10-29 22:02:24 +0800

    Create Day 052.java

---
+++ Day 052.java
@@ -0,0 +1,10 @@
+public class Day052 {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 3) {
+            System.out.println("Belajar Java ke-" + i);
+            i++;
+        }
+    }
+}
