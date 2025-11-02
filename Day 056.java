bc4570e7fb7018854bc0129156a1d16d16002de0
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-11-02 20:31:16 +0800

    Create Day 056.java

---
+++ Day 056.java
@@ -0,0 +1,10 @@
+public class Day056 {
+    public static void main(String[] args) {
+        int i = 3;
+
+        do {
+            System.out.println("Angka: " + i);
+            i++;
+        } while (i <= 7);
+    }
+}
