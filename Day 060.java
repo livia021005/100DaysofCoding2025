4b2738b290c0220fd409f1f116469f7469b0bbd1
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-11-06 21:58:51 +0800

    Create Day 060.java

---
+++ Day 060.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Day060 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N: ");
+        int N = input.nextInt();
+
+        System.out.println("\nAngka ganjil dari " + N + " sampai 1:");
+        for (int i = N; i >= 1; i--) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println("\n\nAngka genap dari " + N + " sampai 1:");
+        for (int i = N; i >= 1; i--) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        input.close();
+    }
+}
