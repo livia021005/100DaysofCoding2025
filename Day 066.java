c5eb5c0d31014d545486ab3abcde7298257918a0
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-11-12 21:49:38 +0800

    Create Day 066.java

---
+++ Day 066.java
@@ -0,0 +1,9 @@
+public class Day066 {
+    public static void main(String[] args) {
+        int N = 5;
+
+        for (int i = 1; i <= N; i++) {
+            System.out.println("*");
+        }
+    }
+}
