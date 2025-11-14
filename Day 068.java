4d2aa389133a7103654164b3f2a3b6ac25133882
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-11-14 21:43:39 +0800

    Create Day 068.java

---
+++ Day 068.java
@@ -0,0 +1,12 @@
+public class Day068 {
+    public static void main(String[] args) {
+        int N = 4; 
+
+        for (int i = 1; i <= N; i++) {  
+            for (int j = 1; j <= N; j++) { 
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
