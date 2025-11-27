254938639fb9231a075a91485d1a2b762785350a
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-11-27 22:41:49 +0800

    Create Day 081.java

---
+++ Day 081.java
@@ -0,0 +1,11 @@
+public class Day081 {
+    public static void main(String[] args) {
+
+        int[] angka = {5, 10, 15, 20, 25};
+
+        System.out.println("Elemen pertama (index 0): " + angka[0]);
+        System.out.println("Elemen ketiga (index 2): " + angka[2]);
+        System.out.println("Elemen terakhir (index 4): " + angka[4]);
+
+    }
+}
