0374988fbe33b347691011c9f2599b623d12a6ef
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-12-04 20:21:44 +0800

    Create Day 089.java

---
+++ Day 089.java
@@ -0,0 +1,15 @@
+public class Day089 {
+
+    static void salam() {
+        System.out.println("Halo Oliv, selamat belajar Java!");
+    }
+
+    static void tampilkanNama(String nama) {
+        System.out.println("Nama kamu adalah: " + nama);
+    }
+
+    public static void main(String[] args) {
+        salam();
+        tampilkanNama("Oliv");
+    }
+}
