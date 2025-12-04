19559ab513754031c4f4a87cdcbf08c91f64e440
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-12-04 20:39:45 +0800

    Create Day 090.java

---
+++ Day 090.java
@@ -0,0 +1,11 @@
+public class Day090 {
+
+    static int kali(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        int hasil = kali(4, 2);
+        System.out.println("Hasil: " + hasil);
+    }
+}
