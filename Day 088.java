c03ef4642cd231ecbc10ec68b0fe4e213ef7b3da
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-12-04 20:06:06 +0800

    Create Day 088.java

---
+++ Day 088.java
@@ -0,0 +1,12 @@
+public class Day088 {
+    public static void main(String[] args) {
+
+        int[] angka = {10, 20, 30, 40, 50};
+
+        System.out.println("Menampilkan elemen array menggunakan for-each:");
+
+        for (int nilai : angka) {
+            System.out.println(nilai);
+        }
+    }
+}
