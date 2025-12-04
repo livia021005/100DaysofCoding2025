0f0dbbd88483c6a19a42cbed4c502d0b9b9d0a06
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-12-04 20:49:30 +0800

    Create Day 091.java

---
+++ Day 091.java
@@ -0,0 +1,11 @@
+public class Day091 {
+
+    public static void sapaPengguna(String nama) {
+        System.out.println("Halo " + nama + ", selamat datang!");
+    }
+
+    public static void main(String[] args) {
+        sapaPengguna("Oliv");
+        sapaPengguna("Rina");
+    }
+}
