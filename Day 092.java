da131c21605bb44cbf732793a49763c7a870fc60
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-12-08 20:04:30 +0800

    Create Day 092.java

---
+++ Day 092.java
@@ -0,0 +1,11 @@
+public class Day090 {
+
+    static int tambah(int a, int b) {
+        return a + b;  
+    }
+
+    public static void main(String[] args) {
+        int hasil = tambah(3, 7);
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+}
