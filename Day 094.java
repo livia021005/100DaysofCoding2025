004527a3a1dd5c7d80af4c03da386379d2ac2bb0
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-12-10 20:36:51 +0800

    Create Day 094.java

---
+++ Day 094.java
@@ -0,0 +1,10 @@
+public class Day094 {
+    
+    public static void tampilPesan() {
+        System.out.println("haloo olivvv, ini adalah method void!");
+    }
+
+    public static void main(String[] args) {
+        tampilPesan();
+    }
+}
