b4d824a7a813254c45bad6364495a7c0acd4be9a
livia021005 <146793830+livia021005@users.noreply.github.com>
2025-12-11 19:05:14 +0800

    Create Day 095.java

---
+++ Day 095.java
@@ -0,0 +1,11 @@
+public class Day095 {
+
+    public static void cetakPesan(String pesan) {
+        System.out.println(pesan);
+    }
+
+    public static void main(String[] args) {
+        cetakPesan("Halo Oliv, ini pesan pertama!");
+        cetakPesan("Belajar Java itu menyenangkan.");
+    }
+}
